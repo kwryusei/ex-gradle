74893237493c7a5d27fffbbf796c783ce34b24d6
Kawahara Ryusei <e225701@ie.u-ryukyu.ac.jp>
2022-11-15 00:19:36 +0900

    fix

--- app/src/test/java/jp/ac/uryukyu/ie/e225701/EnemyTest.java
+++ app/src/test/java/jp/ac/uryukyu/ie/e225701/EnemyTest.java
@@ -6,7 +6,7 @@ package jp.ac.uryukyu.ie.e225701;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class AppTest {
+class EnemyTest {
     @Test 
     void attackTest() {
     int defaultHeroHp = 100;
